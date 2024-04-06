0794a8ebb40e24f120608c81c46dc9aa32694bb9
Sebastien <sebastien.saigo@gmail.com>
2024-04-06 09:36:12 +0200

    Increased version to 5

--- components/main.js
+++ components/main.js
@@ -6,7 +6,7 @@ import { settingsComponent } from './settings.js';
 import { pagesController } from '../pagesController.js';
 import { timeline } from '../lib/timeline.js';
 
-const VERSION = 4;
+const VERSION = 5;
 
 class MainComponent {
     constructor() {
